846de6b53d6261993b8d9d2eb01ddb3847b92ac4
Chris Ranc <clranc94@gmail.com>
2018-09-20 10:36:10 -0400

    pushing current edits to work on desktop

--- Heap.c
+++ Heap.c
@@ -1,12 +1,12 @@
 #include <stdlib.h>
 #include "Heap.h"
 
-heap_t * newHeap(int (*cmp)(node_val, node_val)){
+heap_t * newHeap(int (*doSwap)(void *, void *)){
     heap_t * new_heap = malloc(sizeof(heap_t));
     new_heap->size = 0;
     new_heap->node_cnt = 0;
     new_heap->level_cnt = 0;
-    new_heap->cmp = cmp;
+    new_heap->doSwap = doSwap;
 
     return new_heap;
 }
@@ -14,7 +14,7 @@ heap_t * newHeap(int (*cmp)(node_val, node_val)){
 static inline void increaseHeap(heap_t * heap){
     heap->level_cnt++;
     heap->size += 1 << heap->level_cnt;
-    heap->elem = realloc(heap->elem, heap->size * sizeof(node_val));
+    heap->elem = realloc(heap->elem, heap->size * sizeof(void *));
 }
 
 static inline void decreaseHeap(heap_t * heap){
@@ -25,10 +25,25 @@ static inline void decreaseHeap(heap_t * heap){
             free(heap->elem);
             return;
 
-        heap->elem = realloc(heap->elem, (1 << heap->level_cnt) * sizeof(node_val) + sizeof(heap->elem));
+        heap->size -= 1 << heap->level_cnt;
+        heap->elem = realloc(heap->elem, heap->size * sizeof(void *));
     }
 }
 
-void heapInsert(heap_t * heap, node_val val){
+static inline void nodeSwap(
+
+static inline void heapReorder(heap_t * heap){
     
+
+}
+
+void heapInsert(heap_t * heap, void * val){
+
+    if (heap->node_cnt == heap->size)
+        increaseHeap(heap);
+
+    heap->elem[heap->node_cnt] = val;
+
+
+
 }
--- Heap.h
+++ Heap.h
@@ -3,26 +3,21 @@
 
 #include "generics.h"
 
-typedef struct heap_node_struct{
-    void * value;
-    void * key;
-}h_node_t;
 
 #define H_L_CHILD(x) x * 2 + 1
 #define H_R_CHILD(x) x * 2 + 2
 #define H_PARENT(x) (x - 1) / 2
 
-
 typedef struct heap_struct
 {
     long int size, node_cnt, level_cnt;
-    node * elems;
-    int (*cmp)(*h_node_t, *h_node_t);
-    
+    void ** elem;
+    int (*doSwap)(void *, void *);
+
 } heap_t;
 
-heap_t * newHeap(int (*cmp)(*h_node_t, *h_node_t));
+heap_t * newHeap(int (*cmp)(h_node_t, h_node_t));
 
-void heapInsert(heap_t * heap, h_node_t * val);
+void heapInsert(heap_t * heap, void * val);
 
-
+#endif
